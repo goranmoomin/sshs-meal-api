ba4687fb79aac38f5871c63c9f123593ea791bcf
Sungbin Jo <pcr910303@icloud.com>
2018-09-06 02:21:49 +0900

    remove id route functionality

--- app/route/index.js
+++ app/route/index.js
@@ -1,6 +1,6 @@
 const Router = require("koa-router");
 const router = new Router();
 
-router.get("/", require("./monthly.js")).get("/:id", require("./id.js").route);
+router.get("/", require("./monthly.js"));
 
 module.exports = router;
--- app/route/monthly.js
+++ app/route/monthly.js
@@ -1,6 +1,23 @@
 const helpers = require("../helpers/index.js");
-const { getMealDataAsObject } = require("./id.js");
 const { forEach } = require("p-iteration");
+
+const getMealDataAsObject = async (ctx, id) => {
+    const dom = await ctx.JSDOM.fromURL("http://sshs.hs.kr/75054/subMenu.do/dggb/module/mlsv/selectMlsvDetailPopup.do?mlsvId=" + id);
+    const data = dom.serialize();
+
+    let dataFromHTMLAsArray = helpers.getRegexCaptureAsArray({
+        str: data,
+        regexp: /<td class="ta_l">\s*([^\s]+[^<>]*[^\s]+)\s*<\/td>/g
+    }).reduce((acc, val) => acc.concat(val), []);
+
+    return {
+        type: dataFromHTMLAsArray[2],
+        menu: dataFromHTMLAsArray[3].replace(/&amp;/g, "&").split(","),
+        calories: dataFromHTMLAsArray[4]
+    };
+};
+
+
 
 module.exports = async ctx => {
     const dom = await ctx.JSDOM.fromURL("http://sshs.hs.kr/75054/subMenu.do");
