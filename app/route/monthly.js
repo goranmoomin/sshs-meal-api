8c5e184512fdb023fb348cf6fdbe92ab99466080
Sungbin Jo <pcr910303@icloud.com>
2018-09-07 09:53:47 +0900

    Refactor getting data from captured array code using p-iteration

--- app/route/monthly.js
+++ app/route/monthly.js
@@ -1,5 +1,8 @@
+
 const helpers = require("../helpers/index.js");
-const { forEach } = require("p-iteration");
+const { instanceMethods } = require("p-iteration");
+
+Object.assign(Array.prototype, instanceMethods);
 
 const getMealDataAsObject = async (ctx, id) => {
     const dom = await ctx.JSDOM.fromURL("http://sshs.hs.kr/75054/subMenu.do/dggb/module/mlsv/selectMlsvDetailPopup.do?mlsvId=" + id);
@@ -22,26 +25,15 @@ const getMealDataAsObject = async (ctx, id) => {
 module.exports = async ctx => {
     const dom = await ctx.JSDOM.fromURL("http://sshs.hs.kr/75054/subMenu.do");
     const data = dom.window.document.querySelector("tbody").outerHTML;
-    ctx.message = [];
 
-    await forEach(helpers.getRegexCaptureAsArray({
+    ctx.message = (await helpers.getRegexCaptureAsArray({
         str: data,
         regexp: /<td.*?>\s*(\d+)\s*<ul>\s*([^]*?)\s*<\/ul><\/td>/g
-    }), async (element, index, array) => {
-        let tmpObject = {};
-
-        await forEach(helpers.getRegexCaptureAsArray({
+    }).asyncMap(async (element, index, array) => ({
+        date: parseInt(element[0]),
+        menu: await helpers.getRegexCaptureAsArray({
             str: element[1],
             regexp: /<li>\s*<a href="javascript:void\(0\);" onclick="fnDetail\('(\d+)'\);"><span class="ico_schFood"><\/span>(.+?)<\/a>\s*<\/li>\s*/g
-        }), async (element, index, array) => {
-            tmpObject[element[1]] = await getMealDataAsObject(ctx, parseInt(element[0]));
-        });
-
-        if(Object.getOwnPropertyNames(tmpObject) != 0) {
-            ctx.message.push({
-                date: parseInt(element[0]),
-                menu: tmpObject
-            });
-        }
-    });
+        }).asyncMap(async (element, index, array) => await getMealDataAsObject(ctx, parseInt(element[0]))) 
+    }))).filter(element => element.menu.length != 0);
 };
